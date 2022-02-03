7c2ec4134ac0104c02a09881dc2ce079e7aa1bb6
João Victor Corado <jhonisilva545@gmail.com>
2022-02-03 15:35:23 -0300

    Casas Decimais

--- calculadora.js
+++ calculadora.js
@@ -11,16 +11,16 @@ mulher ==2;
 function calculoHomem(){
 
     if(idade.value <= 18 ){
-        resultado.innerHTML = (17.686 * parseInt(peso.value) + 658.2)
+        resultado.innerHTML = (17.686 * parseInt(peso.value) + 658.2).toFixed(3);
     }
     else if(parseInt(idade.value)  <=30){
-        resultado.innerHTML = (15.057 * parseInt(peso.value) + 692.2)
+        resultado.innerHTML = (15.057 * parseInt(peso.value) + 692.2 ).toFixed(3)
     }
     else if(parseInt(idade.value)  <=60){
-        resultado.innerHTML = (11.472 * parseInt(peso.value) + 873.1)
+        resultado.innerHTML = (11.472 * parseInt(peso.value) + 873.1).toFixed(3)
     }
     else if(parseInt(idade.value)  >=61){
-        resultado.innerHTML = (11.711 * parseInt(peso.value) + 587.7)
+        resultado.innerHTML = (11.711 * parseInt(peso.value) + 587.7).toFixed(3)
     }
    
 }
@@ -28,16 +28,16 @@ function calculoHomem(){
 function calculoMulher(){
 
     if(idade.value <= 18 ){
-        resultado.innerHTML = (13.384 * parseInt(peso.value) + 692.6)
+        resultado.innerHTML = (13.384 * parseInt(peso.value) + 692.6).toFixed(3)
     }
     else if(parseInt(idade.value)  <=30){
-        resultado.innerHTML = (14.818 * parseInt(peso.value) + 486.6)
+        resultado.innerHTML = (14.818 * parseInt(peso.value) + 486.6).toFixed(3)
     }
     else if(parseInt(idade.value)  <=60){
-        resultado.innerHTML = (8.126 * parseInt(peso.value) + 845.6)
+        resultado.innerHTML = (8.126 * parseInt(peso.value) + 845.6).toFixed(3)
     }
     else if(parseInt(idade.value)  >=61){
-        resultado.innerHTML = (9.082 * parseInt(peso.value) + 658.5)
+        resultado.innerHTML = (9.082 * parseInt(peso.value) + 658.5).toFixed(3)
     }
    
 }
